dc6cb06b44b3ff347c0fa6c0c7b5449ac6efef15
Jlll1 <arghantentua@tutanota.com>
2022-05-30 14:39:36 +0200

    Add flameshot cmd

--- void-packages/dwm/files/config.h
+++ void-packages/dwm/files/config.h
@@ -55,13 +55,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL }; 
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL }; 
+static const char *termcmd[]       = { "alacritty", NULL };
+static const char *flameshotcmd[]  = { "flameshot", "gui" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
