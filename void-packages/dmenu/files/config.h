494e0dffee18b39ed1ae9e1e798a3113c4037ae0
Jlll1 <arghantentua@tutanota.com>
2022-07-23 15:50:07 +0200

    Sync

--- void-packages/dmenu/files/config.h
+++ void-packages/dmenu/files/config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=11"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#fbf1c7", "#323027" },
-	[SchemeSel] = { "#323027", "#fbf1c7" },
+	[SchemeNorm] = { "#ece1d7", "#2a2520" },
+	[SchemeSel] = { "#2a2520", "#ebc06d" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
